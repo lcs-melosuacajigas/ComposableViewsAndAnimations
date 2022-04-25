357c1867d66bb2e791aa5a6daacd24d462a0a331
Mateo Elosua <melosua@lcs.on.ca>
2022-04-25 14:26:37 -0400

    Composable animation assignment circle moves from left to right

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct CustomComposableView: View {
+    
+    // MARK: Stored properties
+    @State private var offset = 0.0
+    
+    
+    //MARK: Computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        ZStack    {
+            Circle()
+                .frame(width: 50, height: 50)
+            Text("Ok")
+                .foregroundColor(.white)
+        }
+        .offset(x: offset, y: 0)
+        .animation(.default)
+        .onTapGesture {
+            // Move the circle to the right
+            offset = 100.0
+        }
+     }
+  }
 
 struct CustomComposableView_Previews: PreviewProvider {
     static var previews: some View {
         CustomComposableView()
     }
-}
+  }
+
